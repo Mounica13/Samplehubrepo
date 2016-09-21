7fd23c294902e66b3095a13757302f7f3cffa2e9
iyussuf <mounidev13@gmail.com>
2016-09-21 14:31:34 +0530

    S-01004

--- Tripattern/Program.cs
+++ Tripattern/Program.cs
@@ -13,7 +13,7 @@ namespace Tripattern
 
             int val = 5;
             int i, j, k;
-            for (i = 5; i >=1; i--)
+            for (i = 1; i<=val; i++)
             {
                 for (j = 1; j <= val ; j++)
                 {
